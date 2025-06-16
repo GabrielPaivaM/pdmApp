7d8c8932a145cc03c7fa05bf0f6eb69447c9591a
Gabriel Paiva <127259026+GabrielPaivaM@users.noreply.github.com>
2025-06-15 21:35:37 -0300

    Update firebaseConfig.js

--- src/firebaseConfig.js
+++ src/firebaseConfig.js
@@ -2,12 +2,7 @@ import { initializeApp } from 'firebase/app';
 import { getFirestore } from 'firebase/firestore';
 
 const firebaseConfig = {
-    apiKey: "AIzaSyCVRFa_NgWoLWFWDp4XXSW9jpEmJGX-g1g",
-    authDomain: "pdmassinaturas.firebaseapp.com",
-    projectId: "pdmassinaturas",
-    storageBucket: "pdmassinaturas.firebasestorage.app",
-    messagingSenderId: "960423299398",
-    appId: "1:960423299398:web:134055dd8986b65a767d47"
+    
 };
 
 const app = initializeApp(firebaseConfig);
